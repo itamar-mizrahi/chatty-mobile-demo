1434cc3bd6f62adff7187e49df498a80bd8ae569
Itamar Mizrahi <48559693+itamar-mizrahi@users.noreply.github.com>
2023-04-17 02:08:41 +0300

    expo update

--- client/App.js
+++ client/App.js
@@ -6,7 +6,8 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>{outputText}</Text>
-      <Button title="like" onPress={()=> setOutputText('next')}/>
+      <Text>איתמר</Text>
+      <Button title="like" onPress={()=> setOutputText('hello')}/>
     </View>
     
   );
